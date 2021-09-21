2d246cd62461b2473a7e65d21a81f312ca3229c4
k0ccc <45402819+k0ccc@users.noreply.github.com>
2021-09-21 13:47:20 +0300

    Update random_short.py

--- random_short.py
+++ random_short.py
@@ -1,15 +1,27 @@
 import random
 import string
 import webbrowser
+import requests
+
 
 def gen_shorturl():
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
 
 def short_main():
-    i = input('How many tabs?')
+    i = input('How many tabs? ')
     o = 0
+    # try:
     while o < int(i):
         o += 1
-        webbrowser.open_new_tab('https://clck.ru/'+gen_shorturl())
+        url = 'https://clck.ru/'+gen_shorturl()
+        response = requests.get(url)
+        if response.status_code == 200:
+            webbrowser.open_new_tab(url)
+            print(url + ' существует')
+        else:
+            print(url + ' не существует')
+    # except:
+    #     pass
+    print("DONE")
 if __name__ == "__main__":
     short_main()
