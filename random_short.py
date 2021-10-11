46f38c0937f941214fb73f9b527a843069849ebc
k0ccc <45402819+k0ccc@users.noreply.github.com>
2021-10-11 15:28:01 +0300

    Update random_short.py

--- random_short.py
+++ random_short.py
@@ -4,24 +4,57 @@ import webbrowser
 import requests
 
 
-def gen_shorturl():
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
+def gen_shorturl(number):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(number))
 
-def short_main():
-    i = input('How many tabs? ')
+# Допилить кнопку назад, сделать что-то с шортюрл, и oomoney
+def site_get(site, url_aft_site):
+    i = int(input('How many tabs? '))
     o = 0
-    # try:
-    while o < int(i):
-        o += 1
-        url = 'https://clck.ru/'+gen_shorturl()
-        response = requests.get(url)
-        if response.status_code == 200:
-            webbrowser.open_new_tab(url)
-            print(url + ' существует')
+    while True:
+        while o < i:
+            o += 1
+            url = site+'/'+gen_shorturl(url_aft_site)
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    webbrowser.open_new_tab(url)
+                    print(url + ' существует')
+
+                # elif site in url:
+                #     print(url + ' ссылаеться сам на себя')
+                else:
+                    print(url + ' не существует')
+
+            except:
+                print("Чтот-то страшное: ", url)
+        print("DONE")
+        repeat = int(input("Закончить = 0, Любое другое кол-во вкладок "))
+        if repeat == 0:
+            break
         else:
-            print(url + ' не существует')
-    # except:
-    #     pass
-    print("DONE")
+            i = repeat
+            o=0
+
+URLs = {1 :"https://www.clck.ru",
+        2 :"https://www.shorturl.at"
+        }
+
+
+aft_site = {"https://www.clck.ru"       :5,
+            "https://www.shorturl.at"   :5
+        }
+
+def main():
+    u = 0
+    for url in URLs.values():
+        u +=1
+        print(u,url[12:])
+    input_number = int(input())
+    for w,i in URLs.items():
+        for o in aft_site.keys():
+            if o in i and input_number == w:
+                site_get(URLs[w],aft_site[i])
+
 if __name__ == "__main__":
-    short_main()
+    main()
